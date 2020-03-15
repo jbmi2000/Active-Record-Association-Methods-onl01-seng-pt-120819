f9af4fc4d250cf9b321acdceb041dbf2121ad68d
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-15 02:18:47 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.where(artists.id ==!nil)
+    artists.name.all
     # return an array of strings containing every musician's name
   end
 end
